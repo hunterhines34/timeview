219c22e3f313179dd622c97c1215a8ebb5f233e0
Hunter Hines <hunterhines34@gmail.com>
2019-11-04 16:25:36 -0600

    Initial upload

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:timeview/services/world_time.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/Detroit', location: 'Detroit', flag: 'usa.png'),
+    WorldTime(url: 'America/Denver', location: 'Denver', flag: 'usa.png'),
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+    WorldTime(url: 'America/Santo_Domingo', location: 'Santo Domingo', flag: 'santo_domingo.png'),
+  ];
+
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDayTime,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[800],
+        title: Text(
+          'Choose Location',
+          style: TextStyle(
+            fontSize: 17.0,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index){
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  updateTime(index);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/pages/loading.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:timeview/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  String time = 'Loading time data...';
+
+  void setupWorldTime() async{
+    WorldTime instance = WorldTime(location: 'Germany', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDayTime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitWave(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      ),
+    );
+  }
+}
